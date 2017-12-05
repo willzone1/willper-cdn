00b3bc855b230b448f0c8a08bb4ce79c00c9b99d
Will Persampieri <wpersampieri@gmail.com>
2017-12-04 22:33:15 -0500

    simple hello world

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import cgitb
 import random
-import YourFormProcessor
+#import YourFormProcessor
 import django
 
-cgitb.enable() # Will catch tracebacks and errors for you. Comment it out if you no-longer need it.
+#gitb.enable() # Will catch tracebacks and errors for you. Comment it out if you no-longer need it.
 
 if __name__ == '__main__':
-  print "Hello World!"
+	print("Hello World!")
