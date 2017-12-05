8520bcccb0a274ebd7fdec581cef5a46cef24021
Will Persampieri <wpersampieri@gmail.com>
2017-12-04 21:36:26 -0500

    webpage redirect

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
-import os
+import cgitb
+import random
+import YourFormProcessor
 
-print "Hello World"
+cgitb.enable() # Will catch tracebacks and errors for you. Comment it out if you no-longer need it.
+
+if __name__ == '__main__':
+  YourFormProcessor.Process_Form() # This is your logic to process the form.
+
+  redirectURL = "/?r=%s" % random.randint(0,100000000)
+
+  print 'Content-Type: text/html'
+  print 'Location: %s' % redirectURL
+  print # HTTP says you have to have a blank line between headers and content
+  print '<html>'
+  print '  <head>'
+  print '    <meta http-equiv="refresh" content="0;url=%s" />' % redirectURL
+  print '    <title>You are going to be redirected</title>'
+  print '  </head>' 
+  print '  <body>'
+  print '    Redirecting... <a href="%s">Click here if you are not redirected</a>' % redirectURL
+  print '  </body>'
+  print '</html>'
