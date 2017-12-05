fbf81dbc1b1b1046875c7885eb9ab9502163bcf6
Will Persampieri <wpersampieri@gmail.com>
2017-12-04 22:54:36 -0500

    simple hello world

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import main
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
+	main
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 	#TODO: write code that redirects
