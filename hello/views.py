49ffb399307cd8117468c4e69556aa3ea85841be
Will Persampieri <wpersampieri@gmail.com>
2017-12-04 23:32:53 -0500

    getting IP???

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.gis.utils import GeoIP
+from django.template import  RequestContext
+from django.shortcuts import render_to_response
 import Willmain
 
 from .models import Greeting
@@ -7,6 +10,8 @@ from .models import Greeting
 # Create your views here.
 def index(request):
 	# return HttpResponse('Hello from Python!')
+	IP = get_flient_ip(request)
+	print(IP)
 	Willmain.main()
 	return render(request, 'index.html')
 	#TODO: write code that redirects
@@ -21,3 +26,10 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
